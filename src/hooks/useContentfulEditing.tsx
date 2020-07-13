f33bf7e82aead0c7db3b9bd8eb94265841939223
Chris D. MacRae <chris@forestry.io>
2020-07-13 15:12:01 -0300

    Remove unused hooks

--- src/hooks/useContentfulEditing.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import { ContentfulEditingContext } from '../providers/ContentfulEditingContext'
-
-export function useContentfulEditing() {
-  return React.useContext(ContentfulEditingContext)
-}
