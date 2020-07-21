f1d1ccc855f2a772f1ed798d63a06aeb57b57a06
Chris D. MacRae <hello@chrisdmacrae.com>
2020-07-21 20:03:19 -0300

    Add a next.js helper for getting static props

--- src/utils/getContentfulPreviewProps.ts
+++ src/utils/getContentfulPreviewProps.ts
@@ -1,23 +1,49 @@
 import { Entry } from 'contentful';
 import { ContentfulClient, ContentfulClientOptions } from '../apis/contentful';
+import ContentfulDeliveryService from '../services/contentful/delivery';
 
 export interface ContentfulPreviewPropsOptions {
   entryId: string,
   clientId: string,
+  environmentId: string,
   spaceId: string,
-  accessToken: string
+  accessToken: string,
+  query?: any
 }
 
-export async function getContentfulPreviewPropsForEntry<TDataType = any>({entryId, clientId, spaceId, accessToken}: ContentfulPreviewPropsOptions) {
+export async function getContentfulPreviewPropsForEntry<TDataType = any>(opts: ContentfulPreviewPropsOptions) {
+  try {
+    const contentful = createContentfulPreviewClient(opts);
+
+    return await contentful[opts.spaceId].delivery.getEntry(opts.entryId, opts.query) as Entry<TDataType>;
+  }
+  catch (err) {
+    throw err;
+  }
+}
+
+export async function getContentfulPreviewPropsForEntries<TDataType = any>(opts: Omit<ContentfulPreviewPropsOptions, "entryId">) {
+  try {
+    const contentful = createContentfulPreviewClient(opts);
+
+    return await ContentfulDeliveryService.GetMany<TDataType>(contentful[opts.spaceId].delivery, opts.query);
+  }
+  catch (err) {
+    throw err;
+  } 
+}
+
+function createContentfulPreviewClient(opts: Omit<ContentfulPreviewPropsOptions, "entryId">): ContentfulClient {
   const clientOpts: ContentfulClientOptions = {
-    clientId: clientId,
-    spaceId: spaceId,
+    clientId: opts.clientId,
+    spaceId: opts.spaceId,
+    defaultEnvironmentId: opts.environmentId,
     accessTokens: {
-      delivery: accessToken,
-      management: ""
+      delivery: opts.accessToken,
+      management: "" // we don't need management in this context
     }
   }
   const contentful = new ContentfulClient(clientOpts);
 
-  return await contentful[spaceId].delivery.getEntry(entryId) as Entry<TDataType>;
+  return contentful;
 }
