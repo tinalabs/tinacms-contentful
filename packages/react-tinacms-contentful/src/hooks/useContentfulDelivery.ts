38af5b7b9daed2e95b180275029b683ca8f6ef9f
Chris D. MacRae <hello@chrisdmacrae.com>
2020-09-24 11:43:32 -0300

    Fix delivery hook

--- packages/react-tinacms-contentful/src/hooks/useContentfulDelivery.ts
+++ packages/react-tinacms-contentful/src/hooks/useContentfulDelivery.ts
@@ -3,5 +3,5 @@ import { useContentful } from './useContentful';
 export function useContentfulDelivery(spaceId?: string) {
   const contentful = useContentful(spaceId);
 
-  return contentful.sdks.previewClient;
+  return contentful.sdks.deliveryClient;
 }
