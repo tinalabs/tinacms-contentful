7ad381e53e6b8a0ce46b16a363eb011b9148829d
Chris D. MacRae <hello@chrisdmacrae.com>
2021-03-02 23:51:40 -0400

    Fix error with serverless overloads

--- packages/next-tinacms-contentful/src/entries.ts
+++ packages/next-tinacms-contentful/src/entries.ts
@@ -1,28 +1,30 @@
 import { Entry as DeliveryEntry } from 'contentful';
 import { Entry as ManagementEntry } from 'contentful-management/dist/typings/entities/entry';
-import { ClientMode, ContentfulClient, ContentfulClientOptions, SpaceOptions, getEntryOptions } from "tinacms-contentful";
+import { ClientMode, ContentfulClient, ContentfulClientOptions, SpaceOptions, getEntryOptions, getEntriesOptions } from "tinacms-contentful";
 
-export type BaseServerOptions<Mode extends ClientMode> = getEntryOptions<Mode>;
-
-export type ServerOptions<Mode extends ClientMode = any> = BaseServerOptions<Mode> & Omit<ContentfulClientOptions, 'clientId' | 'redirectUrl'> & SpaceOptions
+export type ServerOptions<BaseOptions = any> = BaseOptions & Omit<ContentfulClientOptions, 'clientId' | 'redirectUrl'> & SpaceOptions
+export type getServerSideEntryOptions<Mode extends ClientMode = "delivery"> = ServerOptions<getEntryOptions<Mode>>
+export type getServerSideEntriesOptions<Mode extends ClientMode = "delivery"> = ServerOptions<getEntriesOptions<Mode>> 
 
 class ServerSideEntries {
-  public static async getEntry<EntryShape extends any, Mode extends ClientMode>(entryId: string, options: ServerOptions<Mode>): Promise<DeliveryEntry<EntryShape>>
-  public static async getEntry<EntryShape extends any, Mode extends ClientMode = "delivery" | "preview">(entryId: string, options: ServerOptions<Mode>): Promise<DeliveryEntry<EntryShape>>
-  public static async getEntry<EntryShape extends any, Mode extends ClientMode = "management">(entryId: string, options: ServerOptions<Mode>): Promise<ManagementEntry>
-  public static async getEntry<EntryShape extends any, Mode extends ClientMode>(entryId: string, options: ServerOptions<Mode>): Promise<DeliveryEntry<EntryShape> | ManagementEntry> {
+  public static async getEntry<EntryShape extends any, Mode = unknown>(entryId: string, options: getServerSideEntryOptions<"delivery">): Promise<DeliveryEntry<EntryShape>>
+  public static async getEntry<EntryShape extends any, Mode = "management">(entryId: string, options: getServerSideEntryOptions<"management">): Promise<ManagementEntry>
+  public static async getEntry<EntryShape extends any, Mode extends "delivery" | "preview">(entryId: string, options: getServerSideEntryOptions<Mode>): Promise<DeliveryEntry<EntryShape>>
+  public static async getEntry<EntryShape extends any, Mode extends ClientMode = "delivery">(entryId: string, options: getServerSideEntryOptions<Mode>): Promise<DeliveryEntry<EntryShape> | ManagementEntry> {
     const client = new ContentfulClient(options as any);
 
-    return client.getEntry(entryId, options);
+    // todo: fix options type
+    return client.getEntry<EntryShape, Mode>(entryId, options as any);
   }
 
-  public static async getEntries<EntryShape extends any, Mode extends ClientMode>(query: any, options: Omit<ServerOptions<Mode>, 'query'>): Promise<DeliveryEntry<EntryShape>[]>
-  public static async getEntries<EntryShape extends any, Mode extends ClientMode = "delivery" | "preview">(query: any, options: Omit<ServerOptions<Mode>, 'query'>): Promise<DeliveryEntry<EntryShape>[]>
-  public static async getEntries<EntryShape extends any, Mode extends ClientMode = "management">(query: any, options: Omit<ServerOptions<Mode>, 'query'>): Promise<ManagementEntry[]>
-  public static async getEntries<EntryShape extends any, Mode extends ClientMode>(query: any, options: Omit<ServerOptions<Mode>, 'query'>): Promise<DeliveryEntry<EntryShape>[] | ManagementEntry[]> {
+  public static async getEntries<EntryShape extends any, Mode = unknown>(query: any, options: Omit<getServerSideEntriesOptions<"delivery">, 'query'>): Promise<DeliveryEntry<EntryShape>[]>
+  public static async getEntries<EntryShape extends any, Mode = "management">(query: any, options: Omit<getServerSideEntriesOptions<"management">, 'query'>): Promise<ManagementEntry[]>
+  public static async getEntries<EntryShape extends any, Mode extends "delivery" | "preview">(query: any, options: Omit<getServerSideEntriesOptions<Mode>, 'query'>): Promise<DeliveryEntry<EntryShape>[]>
+  public static async getEntries<EntryShape extends any, Mode extends ClientMode = "delivery">(query: any, options: Omit<getServerSideEntriesOptions<Mode>, 'query'>): Promise<DeliveryEntry<EntryShape>[] | ManagementEntry[]> {
     const client = new ContentfulClient(options as any);
 
-    return client.getEntries<EntryShape, Mode>(query, options);
+    // todo: fix options type
+    return client.getEntries<EntryShape, Mode>(query, options as any);
   }
 }
 
